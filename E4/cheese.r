9fd7e43747d0ef701ecfe70e791b3e31f8c8317c
atn <atn@cs.utexas.edu>
2017-03-26 17:53:24 -0500

    E4: cheese

--- E4/cheese.r
+++ E4/cheese.r
@@ -1,1 +1,9 @@
-data = read.csv('cheese.csv')
+library(mosaic)
+library(lme4)
+
+cheese = read.csv('cheese.csv')
+
+cheese$lvol = log(cheese$vol)
+cheese$lprice = log(cheese$price)
+
+m1 = lmer(lvol ~ (1 + lprice| disp) + (1 + lprice | store), data = cheese)
